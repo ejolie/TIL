7b80b3cb128713034b0bc5f024ec97894933e15d
Eunjeong Park <ejisvip@gmail.com>
2018-12-17 02:19:31 +0900

    Victory tutorial

---
+++ react/victory-tutorial/README.md
@@ -0,0 +1,5 @@
+# Victory Tutorial
+A tutorial for Victory used with the Getting Started guide in Victory Docs. Build a stacked bar chart with Victory components.
+
+## Getting Started
+Clone down the repo, run an `npm install` to install dependencies, and then use `npm start` to run the webpack server. The server will run at `localhost:8080`.
---
+++ react/victory-tutorial/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var srcPath = path.join(__dirname, 'src');
+var buildPath = path.join(__dirname, 'dist');
+
+module.exports = {
+  context: srcPath,
+  entry: path.join(srcPath, 'js', 'client.js'),
+  output: {
+      path: buildPath,
+      filename: "bundle.js"
+  },
+  module: {
+      rules: [
+          {
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            query: {
+              presets: ['@babel/preset-react', '@babel/preset-env']
+            }
+          }
+      ]
+  }
+};
