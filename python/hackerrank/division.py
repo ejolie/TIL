5ec3513c4f90b9b81ad648a85de029f1eac89e45
Eunjeong Park <ejisvip@gmail.com>
2019-01-02 22:18:44 +0900

    Move hackerrank to problem-solving repo

--- python/hackerrank/division.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-
-print(a // b) # integer division
-print(a / b)  # float division
--- python/hackerrank/function.py
+++
@@ -1,8 +0,0 @@
-def is_leap(year):
-    leap = False
-    
-    # Write your logic here
-    
-    return leap
-
-year = int(input())
