ce8ae36ebbce2c784c95adb7b922dc5c3454c951
Eunjeong Park <ejisvip@gmail.com>
2019-04-14 02:05:15 +0900

    Finish 13.4.객체상속의 사용

--- docs/javascript/js-oop/prototype_inheritance.js
+++ docs/javascript/js-oop/prototype_inheritance.js
@@ -1,9 +1,46 @@
 var superObj = { superVal: 'super' };
-var subObj = { subVal: 'sub' };
+// 객체 간 상속 old ver. 1 (표준은 아님)
+// var subObj = { subVal: 'sub' };
+// subObj.__proto__ = superObj; 
 
-subObj.__proto__ = superObj; // 객체 간 상속 old ver. (표준은 아님)
+// 객체 간 상속 old ver. 2 (표준)
+var subObj = Object.create(superObj);
+subObj.subVal = 'sub';
+// debugger;
 console.log('subObj.subVal =>', subObj.subVal);
 console.log('subObj.superVal =>', subObj.superVal);
 subObj.superVal = 'sub';
 console.log('subObj.superVal =>', subObj.superVal);
 console.log('superObj.superVal =>', superObj.superVal);
+
+var kim = {
+	name: 'kim',
+	first: 10,
+	second: 20,
+	sum: function() {
+		return this.first + this.second
+	}
+}
+
+// old ver. 2
+var lee = Object.create(kim);
+lee.name = 'lee';
+lee.first = 10;
+lee.second = 10;
+lee.avg = function() {
+	return (this.first + this.second) / 2;
+}
+
+// old ver. 1
+// var lee = {
+// 	name: 'lee',
+// 	first: 10,
+// 	second: 10,
+// 	avg: function() {
+// 		return (this.first + this.second) / 2;
+// 	}
+// }
+// lee.__proto__ = kim;
+
+console.log('lee.sum():', lee.sum());
+console.log('lee.avg():', lee.avg());
